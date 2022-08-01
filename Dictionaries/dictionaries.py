be10c5ea6f8145f9f9c6356dcf063bd7a33de3d8
davefcodes <davefaibil@gmail.com>
2022-08-01 00:29:53 -0400

    learnt about dictionaries

---
+++ Dictionaries/dictionaries.py
@@ -0,0 +1,63 @@
+alien_0 = {'colour' : 'green', 'points': 5}
+print(alien_0['colour'])
+print(alien_0['points'])
+
+
+
+# alien_1 ={'color':'blue', 'points':5}
+# print(alien_1['color'])
+
+# new_points = alien_1['points'] 
+# print(f"You just earned {new_points} points!")
+
+# #Adding new key-value pairs
+# alien_1['x_position'] = 0
+# alien_1['y_position'] = 25
+
+# print(alien_1)
+
+# #Starting with an empty dictionary 
+
+# alien_2 = {}
+
+# alien_2['color'] = 'green'
+# alien_2['points'] = 60
+
+# print(alien_2)
+
+
+# #Modifying Values in a Dictionary 
+# alien_0 = {'color': 'green'}
+# print(f"The alien is {alien_0['color']} ")
+
+# alien_0['color'] = 'yellow'
+# print(f"The alien is {alien_0['color']}")
+
+# #More example
+# alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+# print(f"The orignal x-position: {alien_0['x_position']}")
+
+# if alien_0['speed'] == 'slow':
+#   x_increment = 1
+# elif alien_0['speed'] == 'medium':
+#   x_increment = 2
+# else:
+#   #This must be a fast alien
+#   x_increment = 3
+
+# alien_0['speed'] = 'fast'
+
+# print(alien_0['speed'])
+
+  
+# #The new position is the old position plus the increment
+# alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+# print(f"New x-position: {alien_0['x_position']}")
+
+
+
+# alien_0 = {'name': 'sam', 'age': 2, 'points':5}
+# print(alien_0)
+# del alien_0['points']
+# print(alien_0)
