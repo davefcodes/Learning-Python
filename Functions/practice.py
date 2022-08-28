d31c933f93049d3e62a58d213a00b72c1c88881e
davefcodes <davefaibil@gmail.com>
2022-08-28 15:09:23 -0400

    Working more on dictionary

--- Functions/practice.py
+++ Functions/practice.py
@@ -1,5 +1,5 @@
-8-6. City Names: Write a function called city_country() that takes in the name of a city and its country.
-The function should return a string formatted like this:   "Santiago, Chile"
+# 8-6. City Names: Write a function called city_country() that takes in the name of a city and its country.
+# The function should return a string formatted like this:   "Santiago, Chile"
 
 def city_country(city, country):
     formated = print(f"{city.title()} {country.title()}")
@@ -12,10 +12,10 @@ city_country("accra,", "ghana")
 
 
 
-8-7. Album: Write a function called make_album() that builds a dictionary describing a music album. 
-The function should take in an artist name and an album title, and it should return a dictionary containing these two pieces of information. 
-Use the function to make three dictionaries representing different albums. 
-Print each return value to show that the dictionaries are storing the album information correctly.
+# 8-7. Album: Write a function called make_album() that builds a dictionary describing a music album. 
+# The function should take in an artist name and an album title, and it should return a dictionary containing these two pieces of information. 
+# Use the function to make three dictionaries representing different albums. 
+# Print each return value to show that the dictionaries are storing the album information correctly.
 
 def make_album(name, title, tracks= None):
     """Building a dictionary containing information about an album."""
@@ -36,9 +36,9 @@ print(music)
 music = make_album('yaw tog', 'time', 7)
 print(music)
 
-8-8. User Albums: Start with your program from Exercise 8-7. Write a while loop that allows users to enter an album’s artist and title. 
-Once you have that information, call make_album() with the user’s input and print the dictionary that’s created. 
-Be sure to include a quit value in the while loop.
+#8-8. User Albums: Start with your program from Exercise 8-7. Write a while loop that allows users to enter an album’s artist and title. 
+# Once you have that information, call make_album() with the user’s input and print the dictionary that’s created. 
+# Be sure to include a quit value in the while loop.
 
 def make_album(name, title):
     album_dict = {
@@ -52,12 +52,12 @@ while True:
 
     artist_name = input("\nEnter artist name: ")
     if artist_name == 'q':
-        print('GoodBye')
+        print('GoodBye!')
         break
 
     album_title = input("\nEnter album title: ")
     if album_title == 'q':
-        print('GoodBye')
+        print('GoodBye!')
         break
 
     print("-----------------------------------------")
