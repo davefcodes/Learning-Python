6b2c90345432d767f368d676d91b6be7e689cf55
davefcodes <davefaibil@gmail.com>
2022-08-28 15:07:24 -0400

    Working more on dictionary

--- Functions/practice.py
+++ Functions/practice.py
@@ -1,44 +1,44 @@
-# 8-6. City Names: Write a function called city_country() that takes in the name of a city and its country.
-# The function should return a string formatted like this:   "Santiago, Chile"
+8-6. City Names: Write a function called city_country() that takes in the name of a city and its country.
+The function should return a string formatted like this:   "Santiago, Chile"
 
-# def city_country(city, country):
-#     formated = print(f"{city.title()} {country.title()}")
-#     return formated
-
-
-# city_country("santiago,", "chile")
-# city_country("rio,", "brazil")
-# city_country("accra,", "ghana")
+def city_country(city, country):
+    formated = print(f"{city.title()} {country.title()}")
+    return formated
 
 
+city_country("santiago,", "chile")
+city_country("rio,", "brazil")
+city_country("accra,", "ghana")
 
-#8-7. Album: Write a function called make_album() that builds a dictionary describing a music album. 
-# The function should take in an artist name and an album title, and it should return a dictionary containing these two pieces of information. 
-# Use the function to make three dictionaries representing different albums. 
-# Print each return value to show that the dictionaries are storing the album information correctly.
 
-# def make_album(name, title, tracks= None):
-#     """Building a dictionary containing information about an album."""
-#     album_info = {
-#         'Artist name':name.title(), 
-#         'Album title':title.title()
-#         }
-#     if tracks:
-#         album_info['Tracks'] = tracks
-#     return album_info 
 
-# music = make_album('drake', 'take care')
-# print(music)
+8-7. Album: Write a function called make_album() that builds a dictionary describing a music album. 
+The function should take in an artist name and an album title, and it should return a dictionary containing these two pieces of information. 
+Use the function to make three dictionaries representing different albums. 
+Print each return value to show that the dictionaries are storing the album information correctly.
 
-# music = make_album('wizkid', 'more love, less ego')
-# print(music)
+def make_album(name, title, tracks= None):
+    """Building a dictionary containing information about an album."""
+    album_info = {
+        'Artist name':name.title(), 
+        'Album title':title.title()
+        }
+    if tracks:
+        album_info['Tracks'] = tracks
+    return album_info 
 
-# music = make_album('yaw tog', 'time', 7)
-# print(music)
+music = make_album('drake', 'take care')
+print(music)
 
-# 8-8. User Albums: Start with your program from Exercise 8-7. Write a while loop that allows users to enter an album’s artist and title. 
-# Once you have that information, call make_album() with the user’s input and print the dictionary that’s created. 
-# Be sure to include a quit value in the while loop.
+music = make_album('wizkid', 'more love, less ego')
+print(music)
+
+music = make_album('yaw tog', 'time', 7)
+print(music)
+
+8-8. User Albums: Start with your program from Exercise 8-7. Write a while loop that allows users to enter an album’s artist and title. 
+Once you have that information, call make_album() with the user’s input and print the dictionary that’s created. 
+Be sure to include a quit value in the while loop.
 
 def make_album(name, title):
     album_dict = {
@@ -64,3 +64,4 @@ while True:
 
     album_info = make_album(artist_name, album_title)
     print(album_info )
+
