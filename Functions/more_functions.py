8157761d916191fee2cb3cb4ac26da88e6095f18
davefcodes <davefaibil@gmail.com>
2022-08-28 15:05:36 -0400

    Working more on dictionary

--- Functions/more_functions.py
+++ Functions/more_functions.py
@@ -19,3 +19,7 @@ def make_shirt(size, message='I love Python'):
     print(f"This shirt is a {size} and {message}")
 
 make_shirt("Large")
+
+# 8-5. Cities: Write a function called describe_city() that accepts the name of a city and its country.
+# The function should print a simple sentence, such as Reykjavik is in Iceland. Give the parameter for the country a default value. 
+# Call your function for three different cities, at least one of which is not in the default country.
