678f85c2b0e2cb87769f765bb5eab5ed801f2d79
davefcodes <davefaibil@gmail.com>
2022-08-29 16:16:01 -0400

    Passing a List

---
+++ Functions/Passing a List.py
@@ -0,0 +1,10 @@
+# Passing a List 
+
+def greet_users(names):
+    """Print a simple message to each user in the list."""
+    for name in names:
+        msg = f"Hello, {name.title()}"
+        print(msg)
+
+user_names = ['Kim', 'Dave', 'Sam', 'Chantel']
+greet_users(user_names)
