83243da7b78f433476294d4aac422fefa0efb0e8
davefcodes <davefaibil@gmail.com>
2022-08-30 16:43:48 -0400

    Modifying a List

--- Functions/Modifying a List.py
+++ Functions/Modifying a List.py
@@ -2,7 +2,7 @@
 
 # Designs that need to be printed 
 unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
-completed_models = []
+completed_models = [] 
 
 # Simulate each design until none it left 
 # Move each desing into completed_models after printing 
