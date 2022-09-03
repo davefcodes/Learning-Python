509119e9a8bba0eb17680d879b56e2c93f463792
davefcodes <davefaibil@gmail.com>
2022-09-02 22:50:29 -0400

    passing list

--- Functions/practice list.py
+++ Functions/practice list.py
@@ -22,13 +22,12 @@ def send_messages(unsent_messages, sent_messages=''):
         
     
 send_messages(unsent_messages, sent_messages)
-# print(sent_messages)
+print(sent_messages)
 
 
 
 #8-11. Archived Messages: Start with your work from Exercise 8-10. Call the function send_messages() with a copy of the list of messages. 
 # After calling the function, print both of your lists to show that the original list has retained its messages.
 
-send_messages(unsent_messages[:], sent_messages)
-
+send_messages(unsent_messages[:], sent_messages) # function_name(list_name[:]) slice notation makes a copy of the list to send to the function.
 print(unsent_messages)
