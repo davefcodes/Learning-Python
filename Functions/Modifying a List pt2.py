dcde256297a8f04dfa1ddcf13393c0c96864cf18
davefcodes <davefaibil@gmail.com>
2022-09-03 22:52:25 -0400

    modifying list

--- Functions/Modifying a List pt2.py
+++ Functions/Modifying a List pt2.py
@@ -22,5 +22,5 @@ def show_completed_models(completed_models):
 unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
 completed_models = [] 
 
-print_models(unprinted_designs, completed_models)
+print_models(unprinted_designs[:], completed_models)
 show_completed_models(completed_models)
