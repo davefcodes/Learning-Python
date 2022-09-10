be3ae21241ecfab462562b8d843bea57a8f23225
davefcodes <davefaibil@gmail.com>
2022-09-10 00:04:58 -0400

    mixing positional and arbitary arguments

--- Functions/Mixing Positional and Arbitrary Arguments.py
+++ Functions/Mixing Positional and Arbitrary Arguments.py
@@ -1,6 +1,6 @@
 # Mixing Positional and Arbitrary Arguments
 
-def make_pizza(size, *toppings):
+def make_pizza(size, *toppings):  # the parameter that accepts an arbitrary number of arguments must be placed last in the function definition.
     """Summarize the pizza we are about to make"""
     print(f"\nMaking a {size}-inch pizza with the following toppings:")
 
