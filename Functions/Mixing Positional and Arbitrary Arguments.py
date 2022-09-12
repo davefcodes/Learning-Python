914fedd22821c75587d1d5182b65ec2c8b253a79
davefcodes <davefaibil@gmail.com>
2022-09-12 00:34:14 -0400

    mixing positional and arbitary arguments

--- Functions/Mixing Positional and Arbitrary Arguments.py
+++ Functions/Mixing Positional and Arbitrary Arguments.py
@@ -1,8 +1,10 @@
 # Mixing Positional and Arbitrary Arguments
 
 #Notes:
-# python matches positional and keyword arguments first and then collects any remaining arguments in the final parameter.
-# the parameter that accepts an arbitrary number of arguments must be placed last in the function definition.
+# - python matches positional and keyword arguments first and then collects any remaining arguments in the final parameter.
+# - the parameter that accepts an arbitrary number of arguments must be placed last in the function definition.
+# - args, which collects arbitrary positional arguments 
+
 def make_pizza(size, *toppings): 
     """Summarize the pizza we are about to make"""
     print(f"\nMaking a {size}-inch pizza with the following toppings:")
@@ -15,12 +17,22 @@ make_pizza('12', 'mushrooms', 'green peppers', 'extra cheese')
 
 
 # Using Arbitrary Keyword Arguments
+# Notes:
+# - **kwargs used to collect non-specific keyword arguments.
+# - double asterisks before the parameter cause Python to create an empty dictionary called user_info
 
-def build_user(first, last, **user_info):
+
+def build_user(first, last, **user_info): 
     """Build a function containing everything we know about a user"""
     user_info['first_name'] = first
-    user_info['last_name'] = last
+    user_info['last_name'] = last  
     return user_info
 
-user_profile = build_user('david', 'faibil', location = 'new york', field = 'engineering')
+user_profile = build_user('david', 'faibil', 
+                          location = 'new york', 
+                          field = 'engineering')
 print(user_profile)
+
+user2 = build_user('sam', 'h', 
+                    location = 'new york', 
+                    field = 'sports therapist')
