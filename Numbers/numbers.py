7962bb79dc68e20a3f9f792023e5d57653815fbf
davefcodes <davefaibil@gmail.com>
2022-10-05 20:48:02 -0400

    worked with integers and floats, and learned some of the ways to work with numerical data

---
+++ Numbers/numbers.py
@@ -0,0 +1,27 @@
+#David Faibil 
+#Date: July 4th 2022
+
+#Constant is a varible whose value stays the same throughout the life of a program. 
+#By Nameing the varibale all capital letters
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
+
+## Try It Yourself 
+#Page 28 Excersie
+
+#2-8. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print() calls to see the results. You should create four lines that look like this
+
+print(5+3) 
+print(4*2)
+print(16/2)
+print(10-2)
+
+#2-9. Favorite Number: Use a variable to represent your favorite number. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+favorite_number = 5
+messsage = f"This is my favorite number {favorite_number}" 
+print(messsage)
+
+
+#2-11. Zen of Python: Enter import this into a Python terminal session and skim through the additional principles.
+import this 
